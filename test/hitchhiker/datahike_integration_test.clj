7da602f6c9f802a2880dd2a3955d60015b7cf022
Timo Kramer <fw-git@timokramer.de>
2021-02-21 15:47:12 +0100

    Add integration tests against Datahike

---
+++ test/hitchhiker/datahike_integration_test.clj
@@ -0,0 +1,14 @@
+(ns hitchhiker.datahike-integration-test
+  (:require [clojure.test :refer :all]
+            [datahike.integration-test :as it]))
+
+(def config {:store {:backend :file :path "/tmp/file-test-1"}})
+
+(defn config-record-file-test-fixture [f]
+  (it/integration-test-fixture config)
+  (f))
+
+(use-fixtures :once config-record-file-test-fixture)
+
+(deftest ^:integration config-record-file-test []
+  (it/integration-test config))
